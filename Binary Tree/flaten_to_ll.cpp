95f15ecb74d901c71d39c69250fb2dabed350da1
codercp10 <cpurohitofficial@gmail.com>
2026-03-05 17:32:59 +0530

    (binary-tree):flatten a binary tree into a linked list

---
+++ Binary Tree/flaten_to_ll.cpp
@@ -0,0 +1,33 @@
+// Given the root of a binary tree, flatten the tree into a "linked list":
+
+// The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+// The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    void flatten(TreeNode* root) {
+        TreeNode* cur = root;
+        while(cur){
+            if(cur->left){
+                TreeNode* prev = cur->left;
+                while(prev->right) prev = prev->right;
+                prev->right = cur->right;
+                cur->right = cur->left;
+                cur->left = nullptr;
+            }
+            cur = cur->right;
+        }
+    }
+};
